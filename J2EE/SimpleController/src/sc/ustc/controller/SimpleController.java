2599d1fd529f3304b6ab01f518d79b2131d229da
PCM <pcmpcmpcm@qq.com>
2018-12-05 03:57:06 +0800

    finish the first J2EE homework

--- J2EE/SimpleController/src/sc/ustc/controller/SimpleController.java
+++ J2EE/SimpleController/src/sc/ustc/controller/SimpleController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class SimpleController extends HttpServlet {
     public void init(ServletConfig arg0) throws SecurityException{
@@ -21,6 +22,17 @@ public class SimpleController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException{
         System.out.println("发送get请求");
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        String title = "SimpleController";
+        String body = "欢迎使用SimpleController";
+        String docType = "<!DOCTYPE html> \n";
+        out.println(docType +
+                "<html> \n" +
+                "<head><meta charset = \"utf-8\"><title>" + title +"</title></head>\n" +
+                "<body><meta charset = \"utf-8\">" + body + "</body>\n" +
+                "</html>"
+        );
     }
 
 }
--- J2EE/UseSC/web/index.jsp
+++ J2EE/UseSC/web/index.jsp
@@ -1,16 +1,16 @@
 <%--
   Created by IntelliJ IDEA.
-  User: pcmpc
-  Date: 2018/12/4
-  Time: 16:57
+  User: PCM
+  Date: 2018/12/5
+  Time: 1:30
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>SimpleController</title>
+    <title>$Title$</title>
   </head>
   <body>
-  欢迎使用SimpleController
+  $END$
   </body>
 </html>
